d0b7aa6ed7791f74ba968e9ac250bde9ce0e834a
Sri Rajya Lakshmi Vure <55075626+SriRajyaLakshmi38@users.noreply.github.com>
2025-04-28 14:01:27 +0530

    Time: 0 ms (100%), Space: 18.6 MB (76.17%) - LeetHub

--- 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.py
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.py
@@ -3,8 +3,6 @@ class Solution:
         n = len(arr)
         step = n // 4
         for i in range(n - step):
-            print("step is" , step)
-            print(i)
             if arr[i] == arr[i + step]:
                 return arr[i]
 
