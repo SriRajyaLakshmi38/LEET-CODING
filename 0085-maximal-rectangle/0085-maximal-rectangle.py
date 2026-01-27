4de365a0308daee9ed6b31fc6b656885123361c1
Sri Rajya Lakshmi Vure <55075626+SriRajyaLakshmi38@users.noreply.github.com>
2026-01-28 01:23:17 +0530

    Time: 35 ms (44.37%), Space: 24.7 MB (21.61%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,29 +1,33 @@
-from collections import deque
-
 class Solution:
+    def area(self, heights: List[int]) -> int:
+        stack = []
+        maxArea = 0
+        n = len(heights)
+
+        for i in range(n + 1):
+            h = 0 if i == n else heights[i]
+            while stack and h < heights[stack[-1]]:
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                maxArea = max(maxArea, height * width)
+            stack.append(i)
+
+        return maxArea
+
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         if not matrix:
             return 0
-        
+
+        m, n = len(matrix), len(matrix[0])
+        hist = [0] * n
         ans = 0
-        hist = [0] * len(matrix[0])
-        
-        for row in matrix:
-            for i in range(len(row)):
-                hist[i] = hist[i] + 1 if row[i] == '1' else 0
-            ans = max(ans, self.largestRectangleArea(hist))
-        
-        return ans
-    
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        ans = 0
-        stack = deque()
-        
-        for i in range(len(heights) + 1):
-            while stack and (i == len(heights) or heights[stack[-1]] > heights[i]):
-                h = heights[stack.pop()]
-                w = i if not stack else i - stack[-1] - 1
-                ans = max(ans, h * w)
-            stack.append(i)
-        
-        return ans
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    hist[j] += 1
+                else:
+                    hist[j] = 0
+            ans = max(ans, self.area(hist))
+
+        return ans     
